e9107077a0573d4506f953e644b0ffdbbec67ae1
“espelucero” <espe.lucero@gmail.com>
2017-04-24 15:41:53 -0300

    Ejercicio extra de Celsius a Fahrenheit

---
+++ javascript/javascript.js
@@ -0,0 +1,11 @@
+function convertirCelsius(grados){
+   var cel = document.getElementById('fahrenheit').value;
+   var cal = (cel*(9/5))+32;
+
+   var result = document.getElementById('calculate').value = cal;
+
+
+//alert("En grados Fahrenheit es: " + cal);
+  
+}
+
